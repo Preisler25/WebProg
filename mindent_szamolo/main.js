662b77c3f6d7392f025ffcf7d1e67905e41e8276
Preisler25 <preisler.andras@gmail.com>
2023-10-31 00:21:44 +0100

    masod foku working

--- mindent_szamolo/main.js
+++ mindent_szamolo/main.js
@@ -16,3 +16,27 @@ button1.addEventListener('click', () => {
     let temp6 = num1 ** num2;
     display1.innerHTML = `Összeadás: ${temp1} <br>  Kivonás: ${temp2} <br>  Szorzás: ${temp3} <br>  Osztás: ${temp4} <br>  Maradék: ${temp5} <br>  Hatványozás: ${temp6}`;
 });
+
+
+//második feladat:
+
+let inputa = document.querySelector('#input_a');
+let inputb = document.querySelector('#input_b');
+let inputc = document.querySelector('#input_c');
+let display2 = document.querySelector('#disp2');
+
+let Masodfoku = (a, b, c) => {
+    let d = b * b - 4 * a * c;
+    let x1 = (-b + Math.sqrt(d)) / (2 * a);
+    let x2 = (-b - Math.sqrt(d)) / (2 * a);
+    display2.innerHTML = `x1 = ${x1} <br> x2 = ${x2}`;
+}
+
+addEventListener('keydown', (event) => {
+    if (event.key == 'Enter') {
+        let a = parseInt(inputa.value);
+        let b = parseInt(inputb.value);
+        let c = parseInt(inputc.value);
+        Masodfoku(a, b, c);
+    }
+});
