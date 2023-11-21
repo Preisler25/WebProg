70cc2d1f94ac490b3083d98c2fcd799bfc127879
Preisler25 <preisler.andras@gmail.com>
2023-11-21 20:56:04 +0100

    main

--- Bolti/public/main.js
+++
@@ -1,8 +0,0 @@
-import Cart from './cart.js';
-import Termek from './termek.js';
-
-const display = document.querySelector('#kosar');
-const kosar = new Cart(display);
-const termek = new Termek(1, 'Kenyér', 200, 'kenyer.jpg');
-
-kosar.addProduct(termek);
