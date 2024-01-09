4b6bed9926caec41458929fa1953275419c020b4
Preisler25 <preisler.andras@gmail.com>
2024-01-09 09:28:12 +0100

    main

--- gyakorlas/feladat3/main.js
+++ gyakorlas/feladat3/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const todo_list = document.querySelector('#todo_list');
+    todo_list.classList.add('text-white', 'font-bold', 'text-xl', 'list-decimal', 'list-inside', 'bg-gray-800', 'p-4', 'rounded');
 
     const new_todo_inp = document.querySelector('#new_todo_element');
     const new_todo = document.querySelector('#add_todo');
@@ -14,11 +15,13 @@ document.addEventListener('DOMContentLoaded', () => {
         const todo_text = document.createElement('span');
         todo.appendChild(todo_text);
         todo_text.textContent = new_todo_inp.value;
+        todo_text.style.marginRight = '10px';
 
-        const todo_del_btn = document.createElement('button');
-        todo.appendChild(todo_del_btn);
-        todo_del_btn.textContent = 'del';
-        todo_del_btn.addEventListener('click', () => {
+        const todo_done_btn = document.createElement('button');
+        todo.appendChild(todo_done_btn);
+        todo_done_btn.classList.add('btn', 'bg-green-700', 'hover:bg-green-800', 'text-white', 'font-bold', 'py-2', 'px-4', 'rounded');
+        todo_done_btn.textContent = 'Done';
+        todo_done_btn.addEventListener('click', () => {
             todo.remove();
         });
 
